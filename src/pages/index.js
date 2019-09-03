2c941bde4cd85d648e579ef3cb2e9abb9129d011
exvxe <bareiden@gmail.com>
2019-09-03 22:03:58 +0200

    about, services and clients section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,18 @@
 import React from "react"
 import styled, {createGlobalStyle} from "styled-components";
 
-import bg from "../../static/bg.jpg"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faNetworkWired, faProjectDiagram, faServer, faEthernet } from '@fortawesome/free-solid-svg-icons'
+import bg from "../../static/bg.jpg";
+import Flickity from 'flickity';
+import 'flickity/dist/flickity.min.css';
+import GoogleMapReact from 'google-map-react';
 
 const GlobalStyle = createGlobalStyle`
   body {
     margin: 0;
     padding: 0;
+    color: #333;
   }
 `;
 
@@ -161,42 +167,177 @@ const StyledHeading = styled.div`
 `
 
 const StyledContent = styled.div`
-  margin: 0 320px;
+  margin: 0 220px;
+  padding: 100px 260px;
+  text-align: center;
 `
 
-const IndexPage = () => (
-  <GlobalContainer>
-    <GlobalStyle/>
-  <StyledHeader>
-    <header>
-      <StyledNav>
-        <StyledLogo>
-          <span className="text-container">JS</span>
-        </StyledLogo>
-        <ul>
-          <li><a href="#header">O nas</a></li>
-          <li><a href="#header">Usługi</a></li>
-          <li><a href="#header">Klienci</a></li>
-          <li><a href="#header">Kontakt</a></li>
-        </ul>
-      </StyledNav>
-      <StyledHeading>
-        <div className="main">Lorem ipsum dolor sit amet.</div>
-        <div className="sub">Viverra accumsan in nisl nisi scelerisque eu ultrices vitae auctor. Eget felis eget nunc
-          lobortis mattis aliquam faucibus. Semper auctor neque vitae tempus quam. Et pharetra
-          pharetra massa massa ultricies mi quis hendrerit dolor.
-        </div>
-        <div className="btn">
-          <span className="text-container">Dowiedz się więcej</span>
-        </div>
-      </StyledHeading>
-    </header>
-  </StyledHeader>
-  <StyledContent>
-    test
-  </StyledContent>
-  </GlobalContainer>
-)
+const StyledAbout = styled.div`
+  h1 {
+    font-size: 38px;
+    font-weight: 800;
+  }
+  p {
+    color: #666;
+    font-size: 16px;
+    line-height: 31px;
+  }
+`
+
+const StyledServices = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 100px 0;
+  div {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+  }
+  h4 {
+    margin: 20px 0 0 0;
+  }
+  p {
+    margin: 0;
+    font-size: 14px;
+  }
+`
+
+const StyledClients = styled.div`
+  margin: 80px 80px;
+  div {
+    margin-top: 20px;
+    width: 100%;
+  }
+  h2 {
+    width: 100%;
+    font-weight: 800;
+    text-align: left;
+    box-sizing: border-box
+  }
+  p {
+    font-size: 16px;
+  }
+  span {
+    font-size: 18px;
+    width: 100%;
+    text-align: right;
+    display: block;
+  }
+`
+
+const StyledContact = styled.div`
+  width: 100%;
+  height: 500px;
+`
+
+class IndexPage extends React.Component {
+  static defaultProps = {
+    center: {
+      lat: 59.95,
+      lng: 30.33
+    },
+    zoom: 11
+  };
+  componentDidMount() {
+    new Flickity( '.gallery', {
+      cellSelector: 'div',
+      prevNextButtons: false,
+      adaptiveHeight: true,
+      setGallerySize: true
+    });
+  }
+  render () {
+    return (
+      <GlobalContainer>
+        <GlobalStyle/>
+        <StyledHeader>
+          <header>
+            <StyledNav>
+              <StyledLogo>
+                <span className="text-container">JS</span>
+              </StyledLogo>
+              <ul>
+                <li><a href="#header">O nas</a></li>
+                <li><a href="#header">Usługi</a></li>
+                <li><a href="#header">Klienci</a></li>
+                <li><a href="#header">Kontakt</a></li>
+              </ul>
+            </StyledNav>
+            <StyledHeading>
+              <div className="main">Lorem ipsum dolor sit amet.</div>
+              <div className="sub">Viverra accumsan in nisl nisi scelerisque eu ultrices vitae auctor. Eget felis eget nunc
+                lobortis mattis aliquam faucibus. Semper auctor neque vitae tempus quam. Et pharetra
+                pharetra massa massa ultricies mi quis hendrerit dolor.
+              </div>
+              <div className="btn">
+                <span className="text-container">Dowiedz się więcej</span>
+              </div>
+            </StyledHeading>
+          </header>
+        </StyledHeader>
+        <StyledContent>
+          <StyledAbout>
+            <h1>Tempus imperdiet nulla malesuada pellentesque elit eget.</h1>
+            <p>Etiam erat velit scelerisque in. Volutpat est velit egestas dui id ornare arcu odio ut. Non nisi est sit amet facilisis magna etiam tempor orci. Malesuada bibendum arcu vitae elementum curabitur vitae nunc. Vitae tortor condimentum lacinia quis vel eros donec. Id aliquet risus feugiat in ante. Volutpat maecenas volutpat blandit aliquam etiam erat.</p>
+          </StyledAbout>
+          <StyledServices>
+            <div className="container">
+              <FontAwesomeIcon icon={faNetworkWired} size="3x"  color="#333"/>
+              <h4>Nibh cras pulvinar</h4>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.</p>
+            </div>
+            <div className="container">
+              <FontAwesomeIcon icon={faProjectDiagram} size="3x" color="#333"/>
+              <h4>Nibh cras pulvinar</h4>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.</p>
+            </div>
+            <div className="container">
+              <FontAwesomeIcon icon={faServer} size="3x" color="#333"/>
+              <h4>Nibh cras pulvinar</h4>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.</p>
+            </div>
+            <div className="container">
+              <FontAwesomeIcon icon={faEthernet} size="3x" color="#333"/>
+              <h4>Nibh cras pulvinar</h4>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.</p>
+            </div>
+          </StyledServices>
+          <StyledClients>
+            <h2>Nasi klienci</h2>
+            <div className="gallery">
+              <div>
+                <p>"Nunc faucibus a pellentesque sit. Dignissim enim sit amet venenatis urna cursus eget nunc. A pellentesque sit amet porttitor eget dolor morbi non. Adipiscing tristique risus nec feugiat in fermentum posuere urna nec. Sed enim ut sem viverra aliquet."
+                <span>~ John Doe</span>
+                </p>
+              </div>
+              <div>
+                <p>"Nunc faucibus a pellentesque sit. Dignissim enim sit amet venenatis urna cursus eget nunc. A pellentesque sit amet porttitor eget dolor morbi non. Adipiscing tristique risus nec feugiat in fermentum posuere urna nec. Sed enim ut sem viverra aliquet."
+                <span>~ John Doe</span>
+                </p>
+              </div>
+              <div>
+                <p>"Nunc faucibus a pellentesque sit. Dignissim enim sit amet venenatis urna cursus eget nunc. A pellentesque sit amet porttitor eget dolor morbi non. Adipiscing tristique risus nec feugiat in fermentum posuere urna nec. Sed enim ut sem viverra aliquet."
+                <span>~ John Doe</span>
+                </p>
+              </div>
+            </div>
+          </StyledClients>
+          <StyledContact>
+            <GoogleMapReact
+              bootstrapURLKeys={{ key: 'AIzaSyDdYrvzSt_ErHqUjV_gQGss7E0Tg5q3JDw'}}
+              defaultCenter={this.props.center}
+              defaultZoom={this.props.zoom}
+            ></GoogleMapReact>
+          </StyledContact>
+        </StyledContent>
+
+      </GlobalContainer>
+    )
+  }
+
+}
 
 
 
