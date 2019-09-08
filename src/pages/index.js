458c6c4bc623901dfd1f3c0bc4b3774fd285a6df
exvxe <bareiden@gmail.com>
2019-09-08 15:28:42 +0200

    netlify form test v3

--- src/pages/index.js
+++ src/pages/index.js
@@ -398,7 +398,7 @@ class IndexPage extends React.Component {
                     </Map>
               </div>
               <div className="container">
-                <form method="post" action="#" data-netlify="true">
+                <form name="contact" method="post" data-netlify="true">
                   <div className="form-container">
                     <label>
                       Imię
